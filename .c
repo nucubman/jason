004c31556893de0352e3a11d126b0863677f85ee
Luka Nucubidze <lukanucubidze@yahoo.com>
2017-02-17 18:40:12 +0400

    Update and rename .gitignore to .c

---
+++ .c
@@ -0,0 +1,80 @@
+bituri operaciebi  ! ! !
+int a = 60;  0011 1100
+int b = 13;  0000 1101  
+int c = a&b;es & aris bituri 1 cali rocaa! anu a - sa da b- s jami 16 obitshi udris 12s! 
+c = a|b;  am shemtxvevashi aris "an" , anu sxvanairad ikribeba teqsvmetobiti da sxva pasuxs vigebt ! anu an shi 1 sa da 0 is jami aris 1 da & shi aris 0!
+SHIFT - anu chaweva marjvniv da marcxniv ramdeni < ic an > ic gviweria imdenit wevs am baitebs marjvin an marcxniv!
+a << 2; 11 11 0000
+a >> 2; 0000 1111
+
+& = shemtxveashi 1 s vigebt mxolod 1 sa da 1 is jamit!
+^ - qsoris shemxtvevashi 1 s vigebt mxolod 0 sa da 1 is an 1 isa da 0 is jamit! 1+1 = 0; 0+0=0; es qsorshi!
+c = a ^ b; 0011 0001
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
