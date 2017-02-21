6d4f309d14fd86d4e97eceeea254c0a982ec411e
Luka Nucubidze <lukanucubidze@yahoo.com>
2017-02-21 17:42:51 +0400

    Update .c

--- .c
+++ .c
@@ -5,14 +5,18 @@ int c = a&b;es & aris bituri 1 cali rocaa! anu a - sa da b- s jami 16 obitshi ud
 c = a|b;  am shemtxvevashi aris "an" , anu sxvanairad ikribeba teqsvmetobiti da sxva pasuxs vigebt ! anu an shi 1 sa da 0 is jami aris 1 da & shi aris 0!
 SHIFT - anu chaweva marjvniv da marcxniv ramdeni < ic an > ic gviweria imdenit wevs am baitebs marjvin an marcxniv!
 a << 2; 11 11 0000
-a >> 2; 0000 1111
+a >> 2; 0000 1111 1 poziciit chaweva aris 2 ze gayofis tolfasi
 
 & = shemtxveashi 1 s vigebt mxolod 1 sa da 1 is jamit!
 ^ - qsoris shemxtvevashi 1 s vigebt mxolod 0 sa da 1 is an 1 isa da 0 is jamit! 1+1 = 0; 0+0=0; es qsorshi!
 c = a ^ b; 0011 0001
 
-
-
+~ - flip! abrunebs baitebs anu anacvlebs ertmanetshi horizontalurad!
+  
+10000000 = 128;
+01000000 = 64; >> chavwiet marjvniv!
+00100000 = 32;
+00010000 = 16;
 
 
 
